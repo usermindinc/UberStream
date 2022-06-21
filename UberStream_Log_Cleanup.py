a5bae315b8eefc805d307d806d203e3b17c56857
gcoffman@qualtrics.com <gcoffman@qualtrics.com>
2022-06-21 17:31:18 +0000

    Made slight changes to uberstream_log_cleanup job

--- UberStream_Log_Cleanup.py
+++ UberStream_Log_Cleanup.py
@@ -12,11 +12,11 @@ CLEANUP_TTL_VAL = dbutils.widgets.get(CLEANUP_TTL_KEY)
 STREAMING_LOG_TABLES = ["action_stream", "transition_stream", "traveler_stream"]
 
 MAX_OFFSET_PER_PARTITION_QUERY = """
-SELECT group_by_data.topic, group_by_data.partition, group_by_data.max_offset, outer_data.data, outer_data.timestamp, outer_data.processingTimestamp
+SELECT group_by_data.topic, group_by_data.partition, group_by_data.offset, outer_data.timestamp, outer_data.data, outer_data.batchid, outer_data.processingTimestamp, outer_data.processingDate
 FROM streaming_logs.[STREAM_TABLE] outer_data
-INNER JOIN (SELECT topic, partition, MAX(offset) AS max_offset FROM streaming_logs.[STREAM_TABLE]
+INNER JOIN (SELECT topic, partition, MAX(offset) AS offset FROM streaming_logs.[STREAM_TABLE]
 GROUP BY topic, partition) group_by_data
-ON outer_data.offset = group_by_data.max_offset"""
+ON outer_data.offset = group_by_data.offset"""
 STREAM_TABLE_KEY = "[STREAM_TABLE]"
 
 # COMMAND ----------
@@ -25,7 +25,7 @@ def filter_old_rows(table_name, cutoff_timestamp):
     return spark.sql(f"SELECT * FROM streaming_logs.{table_name} WHERE processingTimestamp > '{cutoff_timestamp}'")
 
 def overwrite_streaming_log_table(df, streaming_log_table):
-    df.write.saveAsTable(streaming_log_table, mode="overwrite")
+    df.write.mode("overwrite").saveAsTable(f"streaming_logs.{streaming_log_table}")
 
 def validate_ttl(ttl):
     try:
@@ -51,9 +51,9 @@ for streaming_log_table in STREAMING_LOG_TABLES:
     try:
         print(f"Performing cleanup on table {streaming_log_table} of all entries older than {cutoff_timestamp}")
         filtered_df = filter_old_rows(streaming_log_table, cutoff_timestamp)
-        
+        filtered_df.show()
         filtered_df = ensure_one_entry(streaming_log_table, filtered_df)
-        print(f"Filtered data for table {streaming_log_table}")
+        print(f"Showing Filtered data for table {streaming_log_table} with count {filtered_df.count()}")
         filtered_df.show()
         
         overwrite_streaming_log_table(filtered_df, streaming_log_table)
